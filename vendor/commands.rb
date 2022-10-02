e26c742be024e8c3c340601dcb595189f59bff9c
David <eth3rnit3@gmail.com>
2022-10-02 15:02:51 +0200

    improve rubocop config and add coverage to gitignore

--- vendor/commands.rb
+++ vendor/commands.rb
@@ -6,21 +6,33 @@ def run_commands!
 end
 
 def run_final_commands!
-  run 'bundle exec rubocop . -A'
-
+  run_rubocop_autocorrect!
   setup_jwt_secret!
+  run_db_commands!
+  run_git_commands!
+end
 
+private
+
+def run_rubocop_autocorrect!
+  run 'bundle exec rubocop . -A'
+end
+
+def run_db_commands!
   rails_command("db:drop")
   rails_command("db:create")
   rails_command("db:migrate")
   rails_command("db:seed")
+end
 
+def run_git_commands!
   git :init
+  inject_into_file '.gitignore' do
+    "# Append by eth3rnit3 template\ncoverage\n"
+  end
   git add: "."
   git commit: "-a -m 'Initial commit'"
 end
-
-private
 
 def setup_jwt_secret!
   value = "jwt_secret: #{(0...64).map { ([65, 97].sample + rand(26)).chr }.push(rand(99)).join}"
