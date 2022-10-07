34401f5d0b979afda271a1ca595d9375b48e2952
David <eth3rnit3@gmail.com>
2022-10-07 23:33:03 +0200

    add mysql config

--- vendor/etr_template/docker.rb
+++ vendor/etr_template/docker.rb
@@ -29,12 +29,26 @@ module EtrTemplate
       def configure_database(opt)
         return unless DB_ADAPTERS.include? opt[:database]
 
-        opt[:generator].copy_file 'config/database.yml', force: true
-        opt[:generator].template 'init.sql.erb', 'init.sql'
+        opt[:generator].gsub_file 'config/database.yml', /username:\s\w*$/,
+                                  "username: <%= ENV['DATABASE_USER'] %>"
+        opt[:generator].gsub_file 'config/database.yml', /password:$/,
+                                  "password: <%= ENV['DATABASE_PASSWORD'] %>"
+        opt[:generator].gsub_file 'config/database.yml', /host:\s\w*$/,
+                                  "host: <%= ENV['DATABASE_HOST'] %>"
+        opt[:generator].gsub_file 'config/database.yml', /database:\s\w*$/,
+                                  "database: <%= ENV['DATABASE_NAME'] %>"
+
+        if opt[:database] == 'postgresql'
+          opt[:generator].template 'init.sql.erb', 'init.sql'
+          opt[:generator].gsub_file 'config/database.yml', /#username/, 'username'
+          opt[:generator].gsub_file 'config/database.yml', /#password/, 'password'
+          opt[:generator].gsub_file 'config/database.yml', /#host/, 'host'
+        end
       end
 
       def config_logger(opt)
-        opt[:generator].inject_into_file 'config/application.rb', after: 'class Application < Rails::Application' do
+        opt[:generator].inject_into_file 'config/application.rb',
+                                         after: 'class Application < Rails::Application' do
           "
             logger            = ActiveSupport::Logger.new($stdout)
             logger.formatter  = config.log_formatter
@@ -45,5 +59,3 @@ module EtrTemplate
     end
   end
 end
-
-
